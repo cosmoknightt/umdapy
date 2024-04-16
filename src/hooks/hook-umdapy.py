551047571e334668ac917756dfb02aaf0e935a27
cosmoknight11 <cosmoknight11@gmail.com>
2024-04-16 17:38:06 -0400

    bug fix on github action

--- src/hooks/hook-umdapy.py
+++ src/hooks/hook-umdapy.py
@@ -2,6 +2,6 @@ import umdapy
 from pathlib import Path as pt
 
 hiddenimports = ["umdapy.server", "umdapy.getVersion"]
-icons_path = pt(umdapy.__file__).parent / "../icons/*"
-icons_path = icons_path.resolve().__str__()
-datas = [(icons_path, "icons")]
+icons_dir = pt(umdapy.__file__).parent / "../icons"
+icons_files = [(str(file.resolve()), "icons") for file in icons_dir.glob("*")]
+datas = icons_files
