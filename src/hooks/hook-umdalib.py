248edb8acef0aa2d5b9a47c4f3c1771953bcaab6
cosmoknight11 <cosmoknight11@gmail.com>
2024-04-17 19:22:37 -0400

    Update hook-umdalib.py

--- src/hooks/hook-umdalib.py
+++ src/hooks/hook-umdalib.py
@@ -1,7 +1,7 @@
 import umdalib
 from pathlib import Path as pt
 
-hiddenimports = ["umdalib.server", "umdalib.getVersion"]
+hiddenimports = ["umdalib.server", "umdalib.getVersion", "umdalib.training"]
 icons_dir = pt(umdalib.__file__).parent / "../icons"
 icons_files = [(str(file.resolve()), "icons") for file in icons_dir.glob("*")]
 
