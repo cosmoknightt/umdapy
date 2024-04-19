2318280dd69e5177f2e8a8023123cd64a4ce7da8
cosmoknight11 <cosmoknight11@gmail.com>
2024-04-19 14:49:26 -0400

    Update hook-umdalib.py

--- src/hooks/hook-umdalib.py
+++ src/hooks/hook-umdalib.py
@@ -11,6 +11,7 @@ hiddenimports = [
     "umdalib.server",
     "umdalib.getVersion",
     "umdalib.training",
+    "astrochem_embedding",
 ]
 icons_dir = pt(umdalib.__file__).parent / "../icons"
 icons_files = [(str(file.resolve()), "icons") for file in icons_dir.glob("*")]
