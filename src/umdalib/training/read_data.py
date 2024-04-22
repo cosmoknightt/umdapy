2420af3493a66eb7a35293876fb093bbe4a51dae
cosmoknight11 <cosmoknight11@gmail.com>
2024-04-22 17:10:36 -0400

    Update read_data.py

--- src/umdalib/training/read_data.py
+++ src/umdalib/training/read_data.py
@@ -12,6 +12,27 @@ NPARTITIONS = cpu_count() * 5
 # pbar.register()
 
 
+def read_as_ddf(filetype: str, filename: str, key: str = None, computed=False):
+    ddf = None
+    if filetype == "smi":
+        ddf = dd.read_csv(filename, header=None, names=["SMILES"])
+    elif filetype == "csv":
+        ddf = dd.read_csv(filename)
+    elif filetype == "parquet":
+        ddf = dd.read_parquet(filename)
+    elif filetype == "hdf":
+        ddf = dd.read_hdf(filename, key)
+    elif filetype == "json":
+        ddf = dd.read_json(filename)
+    else:
+        raise ValueError(f"Unknown filetype: {filetype}")
+
+    if computed:
+        with ProgressBar():
+            ddf = ddf.compute()
+    return ddf
+
+
 @dataclass
 class Args:
     filename: str
@@ -21,28 +42,20 @@ class Args:
 
 def main(args: Args):
     print(f"Reading {args.filename} as {args.filetype}")
-    if args.filetype == "csv":
-        df = dd.read_csv(args.filename)
-    elif args.filetype == "parquet":
-        df = dd.read_parquet(args.filename)
-    elif args.filetype == "hdf":
-        df = dd.read_hdf(args.filename, args.key)
-    elif args.filetype == "json":
-        df = dd.read_json(args.filename)
-    else:
-        raise ValueError(f"Unknown filetype: {args.filetype}")
 
-    logger.info(f"read_data file: Shape: {df.shape[0].compute()}")
-    # if df.columns[0] == "Unnamed: 0":
-    #     df = df.drop(columns=["Unnamed: 0"])
+    ddf = read_as_ddf(args.filetype, args.filename, args.key)
+
+    logger.info(f"read_data file: Shape: {ddf.shape[0].compute()}")
+    # if ddf.columns[0] == "Unnamed: 0":
+    #     ddf = ddf.drop(columns=["Unnamed: 0"])
 
     data = {
-        "columns": df.columns.values.tolist(),
+        "columns": ddf.columns.values.tolist(),
     }
 
     with ProgressBar():
-        head = df.head(10)
+        head = ddf.head(10)
         data["head"] = head.to_dict(orient="records")
-        data["shape"] = df.shape[0].compute()
+        data["shape"] = ddf.shape[0].compute()
 
     return data
