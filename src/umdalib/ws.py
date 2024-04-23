212fa29bf7431827c0834a4b3dbf786be7890a8d
cosmoknight11 <cosmoknight11@gmail.com>
2024-04-23 16:35:20 -0400

    Update ws.py

--- src/umdalib/ws.py
+++ src/umdalib/ws.py
@@ -1,6 +1,7 @@
 import asyncio
 import websockets
 from umdalib.utils import logger
+from dataclasses import dataclass
 
 
 async def handle_connection(websocket, path):
@@ -12,13 +13,30 @@ async def handle_connection(websocket, path):
         await websocket.send(f"Hello from umdapy! {message}")
 
 
+@dataclass
 class Args:
     wsport: int
+    action: str
+
+
+server = None
 
 
 def main(args: Args):
+    global server
     logger.info(f"Starting WebSocket server on port {args.wsport}")
-    start_server = websockets.serve(handle_connection, "localhost", args.wsport)
 
-    asyncio.get_event_loop().run_until_complete(start_server)
+    start_server = websockets.serve(handle_connection, "localhost", 8765)
+    server = asyncio.get_event_loop().run_until_complete(start_server)
+
+    if args.action == "stop":
+        stop_websocket_server()
+        return
+
     asyncio.get_event_loop().run_forever()
+
+
+def stop_websocket_server():
+    server.close()
+    asyncio.get_event_loop().run_until_complete(server.wait_closed())
+    logger.info("WebSocket server stopped")
