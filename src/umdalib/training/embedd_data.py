d23bd8f2eff9a0f053e1cbb29ea19934900ed4c1
cosmoknight11 <cosmoknight11@gmail.com>
2024-07-24 15:54:04 -0400

    Update embedd_data.py

--- src/umdalib/training/embedd_data.py
+++ src/umdalib/training/embedd_data.py
@@ -181,10 +181,11 @@ def main(args: Args):
         vec_computed = np.vstack(
             vec_computed
         )  # stack the arrays (n_samples, n_features)
+
         computed_time = f"{(perf_counter() - start_time):.2f} s"
         np.save(embedd_savefile, vec_computed)
 
-    logger.info(f"{vec_computed.shape=}, {len(vec_computed[0])=}, {vec_computed[0]=}")
+    logger.info(f"{vec_computed.shape=}")
 
     logger.info(
         f"Embeddings computed in {(perf_counter() - time):.2f} s and saved to {embedd_savefile.name}"
