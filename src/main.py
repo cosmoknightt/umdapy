56cef4422e51f611d65ed47cf9e4acb947ab115a
cosmoknight11 <cosmoknight11@gmail.com>
2024-08-05 13:54:35 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -15,10 +15,23 @@ class MyClass(object):
 
 
 if __name__ == "__main__":
+    logger.info("Starting main.py")
+    logger.info(f"{sys.argv=}")
     pyfile = sys.argv[1]
     args = None
     if len(sys.argv) > 2:
-        args = json.loads(sys.argv[2])
+        try:
+            if not sys.argv[2].strip():
+                raise ValueError("Input JSON string is empty")
+            args = json.loads(sys.argv[2])
+            logger.info("Successfully loaded JSON string")
+        except json.JSONDecodeError as e:
+            logger.error(f"JSONDecodeError: {e}")
+            sys.exit(1)
+        except ValueError as e:
+            logger.error(f"ValueError: {e}")
+            sys.exit(1)
+        # args = json.loads(sys.argv[2])
 
     logger.info("\n[Received arguments]\n" + json.dumps(args, indent=4))
     logger.info(f"{pyfile=}\n")
