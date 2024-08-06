f76269eab26450d8d95d7441c7f6594a7b39d2f9
cosmoknight11 <cosmoknight11@gmail.com>
2024-08-06 18:50:45 -0400

    Update ml_prediction.py

--- src/umdalib/training/ml_prediction.py
+++ src/umdalib/training/ml_prediction.py
@@ -6,6 +6,7 @@ from typing import TypedDict
 
 from umdalib.training.embedd_data import get_smi_to_vec
 from joblib import load
+from functools import lru_cache
 
 
 class Embedder(TypedDict):
@@ -21,9 +22,15 @@ class Args:
     pretrained_model_file: str
 
 
+@lru_cache()
+def load_model(model_location: str):
+    return load(model_location)
+
+
 def main(args: Args):
 
     logger.info(f"Parsing SMILES: {args.smiles}")
+
     predicted_value = None
     estimator = None
 
@@ -35,16 +42,16 @@ def main(args: Args):
 
     X = smi_to_vector(args.smiles, model)
     logger.info(f"X: {X}")
+    return {"predicted_value": 1}
 
     logger.info(f"Loading estimator from {args.pretrained_model_file}")
-    estimator, scaler = load(args.pretrained_model_file)
+    estimator, scaler = load_model(args.pretrained_model_file)
     if not estimator:
         logger.error("Failed to load estimator")
         raise ValueError("Failed to load estimator")
 
     logger.info(f"Loaded estimator: {estimator}")
     logger.info(f"Loaded scaler: {scaler}")
-    # return {"predicted_value": 1}
 
     predicted_value: np.ndarray = estimator.predict([X])
 
@@ -53,6 +60,7 @@ def main(args: Args):
             predicted_value.reshape(-1, 1)
         ).flatten()
 
+    predicted_value = float(predicted_value[0])
     logger.info(f"Predicted value: {predicted_value}")
 
-    return {"predicted_value": predicted_value}
+    return {"predicted_value": f"{predicted_value:.2f}"}
