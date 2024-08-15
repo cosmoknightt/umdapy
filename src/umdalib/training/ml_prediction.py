49479517f703d0165281cc3047339ca41adcbf79
cosmoknight11 <cosmoknight11@gmail.com>
2024-08-15 17:54:18 -0400

    Update ml_prediction.py

--- src/umdalib/training/ml_prediction.py
+++ src/umdalib/training/ml_prediction.py
@@ -1,12 +1,12 @@
 from dataclasses import dataclass
-
 import numpy as np
+import pandas as pd
 from umdalib.utils import logger
 from typing import TypedDict
-
 from umdalib.training.embedd_data import get_smi_to_vec
 from joblib import load
 from functools import lru_cache
+from pathlib import Path as pt
 
 
 class Embedder(TypedDict):
@@ -20,6 +20,7 @@ class Args:
     smiles: str
     molecular_embedder: Embedder
     pretrained_model_file: str
+    test_file: str
 
 
 @lru_cache()
@@ -33,16 +34,13 @@ def main(args: Args):
 
     predicted_value = None
     estimator = None
+    test_file = pt(args.test_file)
 
     smi_to_vector, model = get_smi_to_vec(
         args.molecular_embedder["name"],
         args.molecular_embedder["file"],
         args.molecular_embedder["pipeline_file"],
     )
-
-    X = smi_to_vector(args.smiles, model)
-    logger.info(f"X: {X}")
-    # return {"predicted_value": 1}
 
     logger.info(f"Loading estimator from {args.pretrained_model_file}")
     estimator, scaler = load_model(args.pretrained_model_file)
@@ -53,12 +51,50 @@ def main(args: Args):
     logger.info(f"Loaded estimator: {estimator}")
     logger.info(f"Loaded scaler: {scaler}")
 
-    predicted_value: np.ndarray = estimator.predict([X])
+    if test_file.exists():
+        logger.info(f"Reading test file: {test_file}")
+        data = pd.read_csv(test_file)
+        logger.info(f"Data shape: {data.shape}")
+
+        columns = data.columns.tolist()
+        if len(columns) == 0:
+            raise ValueError(
+                "Test file should have at least one column with header name SMILES"
+            )
+        if len(columns) > 1:
+            raise ValueError(
+                "Test file should have only one column with header name SMILES"
+            )
+        if columns[0] != "SMILES":
+            raise ValueError("Test file should have a column header named 'SMILES'")
+
+        smiles = data["SMILES"].tolist()
+        X = [smi_to_vector(smi, model) for smi in smiles]
+        logger.info(f"X shape: {len(X)}")
+        if len(X) == 0:
+            raise ValueError("No valid SMILES found in test file")
+
+        predicted_value: np.ndarray = estimator.predict(X)
+    else:
+
+        X = smi_to_vector(args.smiles, model)
+        logger.info(f"X: {X}")
+        predicted_value: np.ndarray = estimator.predict([X])
 
     if scaler:
         predicted_value = scaler.inverse_transform(
             predicted_value.reshape(-1, 1)
         ).flatten()
+
+    if test_file.exists():
+        predicted_value = predicted_value.tolist()
+        data["predicted_value"] = predicted_value
+        savefile = test_file.parent / f"{test_file.stem}_predicted_values.csv"
+        data.to_csv(savefile, index=False)
+
+        logger.info(f"Predicted values saved to {savefile}")
+
+        return {"savedfile": str(savefile)}
 
     predicted_value = float(predicted_value[0])
     logger.info(f"Predicted value: {predicted_value}")
