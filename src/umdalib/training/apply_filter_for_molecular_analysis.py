11e006dd4846eeeb2fdf97f1f3a0102a3b7af2b6
cosmoknight11 <cosmoknight11@gmail.com>
2024-08-19 15:53:44 -0400

    Update apply_filter_for_molecular_analysis.py

--- src/umdalib/training/apply_filter_for_molecular_analysis.py
+++ src/umdalib/training/apply_filter_for_molecular_analysis.py
@@ -4,12 +4,12 @@ import pandas as pd
 from umdalib.training.read_data import read_as_ddf
 from umdalib.utils import logger
 from collections import Counter
-import multiprocessing
+from multiprocessing import Pool, cpu_count
 from pathlib import Path as pt
 from typing import Optional
 
 # Constants for column names
-COLUMN_ATOMS = "'No. of atoms'"
+COLUMN_ATOMS = "No. of atoms"
 COLUMN_ELEMENTS = "Elements"
 COLUMN_IS_AROMATIC = "IsAromatic"
 COLUMN_IS_NON_CYCLIC = "IsNonCyclic"
@@ -25,9 +25,9 @@ def apply_filters_to_df(
 ) -> Optional[pd.Series]:
 
     # Filter based on atomic number
-    if min_atomic_number is not None and x[COLUMN_ATOMS] < min_atomic_number:
+    if min_atomic_number is not None and x[COLUMN_ATOMS] < int(min_atomic_number):
         return None
-    if max_atomic_number is not None and x[COLUMN_ATOMS] > max_atomic_number:
+    if max_atomic_number is not None and x[COLUMN_ATOMS] > int(max_atomic_number):
         return None
 
     # Filter based on elements
@@ -50,6 +50,12 @@ def apply_filters_to_df(
     return x
 
 
+def parallel_apply(df: pd.DataFrame, func, *args) -> pd.DataFrame:
+    with Pool(cpu_count()) as pool:
+        result = pool.starmap(func, [(row, *args) for _, row in df.iterrows()])
+    return pd.DataFrame([r for r in result if r is not None])
+
+
 @dataclass
 class Args:
     analysis_file: str
@@ -61,6 +67,10 @@ class Args:
     filter_structures: list[str]
 
 
+# parallel = True
+parallel = False
+
+
 def main(args: Args):
     analysis_file = pt(args.analysis_file)
     df = pd.read_csv(analysis_file)
@@ -69,19 +79,33 @@ def main(args: Args):
     )
     df["Elements"] = df["Elements"].apply(lambda x: Counter(json.loads(x)))
 
-    final_df: pd.DataFrame = df.apply(
-        apply_filters_to_df,
-        axis=1,
-        args=(
+    if parallel:
+        final_df = parallel_apply(
+            df,
+            apply_filters_to_df,
             args.min_atomic_number,
             args.max_atomic_number,
             args.filter_elements,
             args.filter_structures,
-        ),
-    )
+        )
+    else:
+        final_df: pd.DataFrame = df.apply(
+            apply_filters_to_df,
+            axis=1,
+            args=(
+                args.min_atomic_number,
+                args.max_atomic_number,
+                args.filter_elements,
+                args.filter_structures,
+            ),
+        )
     final_df = final_df.dropna()  # Drop rows that were filtered out
     logger.info("Filtered DataFrame length: %d", len(final_df))
-    filtered_file_path = analysis_file.parent / "filtered_" / analysis_file.name
+    filtered_file_path = (
+        analysis_file.parent / "filtered_" / f"{analysis_file.stem}_filtered.csv"
+    )
+    if not filtered_file_path.parent.exists():
+        filtered_file_path.parent.mkdir(parents=True)
     final_df.to_csv(filtered_file_path)
 
-    return {"filtered_file": analysis_file.parent / "filtered_" / analysis_file.name}
+    return {"filtered_file": str(filtered_file_path)}
