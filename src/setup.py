0b75c384e0d8b9b85edd135162db01c0a813c7ea
cosmoknight11 <cosmoknight11@gmail.com>
2024-08-21 13:31:26 -0400

    version update

--- src/setup.py
+++ src/setup.py
@@ -12,7 +12,7 @@ setup(
         "flask_cors",
         "waitress",
     ],
-    version="1.4.0",
+    version="1.4.1",
     description="umdalib: a Python backend for UMDA_UI",
     author="Aravindh Nivas Marimuthu (Mcguire's group, MIT)",
     license="MIT",
