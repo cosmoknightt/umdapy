b5ba5f861bd3cb72901ba8bcfa6dc3990e61ab47
cosmoknight11 <cosmoknight11@gmail.com>
2024-08-27 20:29:36 -0400

    Update ws.py

--- src/umdalib/ws.py
+++ src/umdalib/ws.py
@@ -4,15 +4,6 @@ from umdalib.utils import logger
 from dataclasses import dataclass
 
 
-async def handle_connection(websocket, path):
-    while True:
-        # Receive message from JavaScript client
-        message = await websocket.recv()
-        logger.info(f"Received from JavaScript: {message}")
-        # Send message back to JavaScript client
-        await websocket.send(f"Hello from umdapy! {message}")
-
-
 @dataclass
 class Args:
     wsport: int
@@ -21,16 +12,45 @@ class Args:
 
 def main(args: Args):
     logger.info(f"Starting WebSocket server on port {args.wsport}")
+    uri = f"ws://localhost:{args.wsport}"
 
     if args.action == "stop":
-        stop_websocket_server()
+        logger.info("Stopping WebSocket server")
+        asyncio.get_event_loop().run_until_complete(stop_server(uri))
         return
 
-    start_server = websockets.serve(handle_connection, "localhost", args.wsport)
-    asyncio.get_event_loop().run_until_complete(start_server)
-    asyncio.get_event_loop().run_forever()
+    logger.info("Starting WebSocket server")
+    asyncio.get_event_loop().run_until_complete(start_server(uri, args.wsport))
 
 
-def stop_websocket_server():
-    asyncio.get_event_loop().stop()
-    logger.info("WebSocket server stopped")
+async def handle_connection(websocket, path) -> None:
+    while True:
+        message = await websocket.recv()
+        logger.info(f"Received from JavaScript: {message}")
+        await websocket.send(f"Hello from umdapy! {message}")
+
+
+async def start_server(uri: str, port: int) -> None:
+    async with websockets.serve(handle_connection, "localhost", int(port)):
+        logger.info(f"WebSocket server started on {uri}")
+        await asyncio.Future()  # run forever
+
+
+async def stop_server(uri: str) -> None:
+    try:
+        async with websockets.connect(uri) as websocket:
+            await websocket.send("STOP")
+            logger.info("Stop signal sent to WebSocket server")
+
+            # Wait for a short time to allow the server to process the stop signal
+            await asyncio.sleep(1)
+            logger.info(f"WebSocket connection closed: {websocket.closed}")
+            # Check if the connection is closed
+            if websocket.closed:
+                logger.info("WebSocket connection closed successfully")
+            else:
+                logger.warning("WebSocket connection is still open")
+    except websockets.exceptions.ConnectionClosed:
+        logger.info("WebSocket connection was closed by the server")
+    except Exception as e:
+        logger.error(f"Error while stopping WebSocket server: {str(e)}")
