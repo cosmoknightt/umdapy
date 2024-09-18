1be1bf00001f4cbbbb54259a6b3eb570e4de42fb
cosmoknight11 <cosmoknight11@gmail.com>
2024-09-17 20:15:40 -0400

    Create y_data_distribution.py

---
+++ src/umdalib/training/y_data_distribution.py
@@ -0,0 +1,54 @@
+import json
+from dataclasses import dataclass
+from pathlib import Path as pt
+import numpy as np
+import pandas as pd
+from umdalib.training.read_data import read_as_ddf
+from umdalib.utils import logger
+
+
+@dataclass
+class Args:
+    filename: str
+    filetype: str
+    key: str
+    use_dask: bool
+    column_name: str
+    training_save_directory: str
+    bin_size: int
+
+
+def main(args: Args):
+    logger.info(f"{args.column_name=}")
+    training_save_directory = pt(args.training_save_directory)
+
+    df = read_as_ddf(
+        args.filetype,
+        args.filename,
+        args.key,
+        use_dask=args.use_dask,
+        computed=True,
+    )
+
+    y: pd.Series = df[args.column_name]
+
+    # Compute histogram data
+    hist, bin_edges = np.histogram(y, bins=int(args.bin_size))
+
+    # Print histogram data
+    print("Histogram counts:", hist)
+    print("Bin edges:", bin_edges)
+
+    histogram_data = {
+        "hist": hist.tolist(),
+        "bin_edges": bin_edges.tolist(),
+    }
+
+    # Save histogram data to JSON file
+    with open(training_save_directory / "histogram_data.json", "w") as f:
+        json.dump(histogram_data, f, indent=2)
+
+    return {
+        "hist": hist.tolist(),
+        "bin_edges": bin_edges.tolist(),
+    }
